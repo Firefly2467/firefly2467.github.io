80f782cd05f33f1797538758b1cf769d7bdbefe5
Firefly2467 <firefly2467@gmail.com>
2018-04-15 20:01:37 -0400

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@
         /* ---------------------------------------------- /*
          * Scroll top
          /* ---------------------------------------------- */
-
+/*
         $(window).scroll(function() {
             if ($(this).scrollTop() > 100) {
                 $('.scroll-up').fadeIn();
@@ -31,7 +31,7 @@
                 $('.scroll-up').fadeOut();
             }
         });
-
+*/
         $('a[href="#totop"]').click(function() {
             $('html, body').animate({ scrollTop: 0 }, 'slow');
             return false;
